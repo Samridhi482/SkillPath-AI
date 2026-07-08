3e00c7d1990c424d26d7ce58859f0f4be4b5cb7d
samridhi <s4378870@gmail.com>
2026-07-08 19:49:48 +0530

    Final commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,11 +2,6 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 const protect = async (req, res, next) => {
-  console.log("========== AUTH DEBUG ==========");
-  console.log("Origin:", req.headers.origin);
-  console.log("Cookies:", req.cookies);
-  console.log("JWT:", req.cookies.jwt);
-
   
   let token = req.cookies.jwt;
 
